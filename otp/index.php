6e2876982380bc865dd7a8c099cf9880e37fbc6c
run-dev <73558390+run-dev@users.noreply.github.com>
2021-06-21 20:35:31 +0700

    Create index.php

---
+++ otp/index.php
@@ -0,0 +1,33 @@
+<?php
+
+  $app_name = $_POST["app"];
+
+  $sender   = $_POST["sender"];
+
+  $messages  = $_POST["message"]; 
+
+  $message = "Login";
+
+$curlSession = curl_init();
+
+curl_setopt($curlSession, CURLOPT_URL, "https://almas-semarang-default-rtdb.asia-southeast1.firebasedatabase.app/auth/otp.json");
+
+curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+$reply = json_decode(curl_exec($curlSession));
+
+curl_close($curlSession);
+
+/*  if ($app_name == "WhatsAuto") {
+
+  if ($sender == "WhatsAuto app") { */
+
+     if ($message == "Login") {
+
+        echo json_encode($reply);
+
+}
+
+?>
